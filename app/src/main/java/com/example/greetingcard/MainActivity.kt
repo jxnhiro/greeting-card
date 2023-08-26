1f81bdeb92971303ad958896f16ac1fde7d4483e
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-27 05:19:47 +0700

    Last Push

---
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+
+class MainActivity : ComponentActivity() {
+//    onCreate is the entry point to this Android app.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+/**
+ * All functions marked with the @Composable annotation
+ * can be called from the setContent() function or from
+ * other Composable functions. The annotation tells the Kotlin
+ * compiler that this function is used by
+ * Jetpack Compose to generate the UI.
+ */
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+//  Composable does not return anything.
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreetingCardTheme {
+        Greeting("Kenny")
+    }
+}
